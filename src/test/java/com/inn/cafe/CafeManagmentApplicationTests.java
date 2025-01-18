ed53940956c6d6415ccfd436e47841893d1eabb5
Nidhal <larbinidhal88@gmail.com>
2025-01-18 19:38:07 +0100

    docker2

--- src/test/java/com/inn/cafe/CafeManagmentApplicationTests.java
+++ src/test/java/com/inn/cafe/CafeManagmentApplicationTests.java
@@ -3,11 +3,8 @@ package com.inn.cafe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CafeManagmentApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   
 
 }
