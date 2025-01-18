ca1f25e7573c9a17906ff9a56d1aa6b6eca73593
Nidhal <larbinidhal88@gmail.com>
2025-01-18 18:36:48 +0100

    sonnar4

--- src/main/java/com.inn.cafe/rest/BillRest.java
+++ src/main/java/com.inn.cafe/rest/BillRest.java
@@ -12,6 +12,7 @@ public interface BillRest {
     @PostMapping(path = "/generateReport")
     public ResponseEntity<String> generateReport(@RequestBody Map<String, Object> requestMap);
 
+
     @GetMapping(path = "/getBills")
     public ResponseEntity<List<Bill>> getBills();
 
